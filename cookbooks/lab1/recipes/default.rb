a3f76d6058f75f964780c8fce5f1f95661d38f8a
Srikanth Bulusu <m814839@A4440489.local>
2016-04-23 18:50:34 -0700

    Chef learning

---
+++ cookbooks/lab1/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: lab1
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'cowsay' do
+	action :install
+end
+
---
+++ cookbooks/lab1/recipes/hello.rb
@@ -0,0 +1,3 @@
+file '/Users/m814839/learning_chef/cookbooks/cookbooks/lab1/recipes/helloworld' do
+	content 'Hello World'
+end
