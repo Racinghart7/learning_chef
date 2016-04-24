2d0ea294ed9fbfacaa25acf240f35a16f89c2c98
Srikanth Bulusu <m814839@A4440489.local>
2016-04-23 19:38:17 -0700

    adding recipe setup.rb

---
+++ cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: workstation
+# Recipe:: setup.rb
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'cowsay' do
+	  action :install
+end
+
+package 'tree' do
+	  action :install
+end
+
+package 'git' do
+	  action :install
+end
+
+file '/etc/motd' do
+	  content 'Property of ...'
+end
